ac73ca9a42be8776f36a219e8979e3ce31ef9b29
Teppei-tt <77786751+Teppei-tt@users.noreply.github.com>
2021-08-22 03:14:00 +0900

    ログイン画面の作成（UI）

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,30 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>LOGINページです。</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={5} borderRadius="sm" shadow="md">
+        <Heading as="h1" size="md" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        {/* Stackは囲った要素を等間隔に配置するときに便利 */}
+        <Stack spacing={5} py={4} px={12}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
